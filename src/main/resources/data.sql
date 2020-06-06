10048cad1a181d0d0fe1216e02464b0a7814e1d4
faviola-26 <faviola_jo@hotmail.com>
2020-06-06 10:08:53 -0500

    finish merging files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 INSERT INTO product_category(id, name, ancestor_id) values (null, 'footware', null);
 INSERT INTO product_category(id, name, ancestor_id) values (null, 'kids', 1);
 INSERT INTO product_category(id, name, ancestor_id) values (null, 'woman', 1);
@@ -8,14 +6,3 @@ INSERT INTO product_category(id, name, ancestor_id) values (null, 'man', 1);
 INSERT INTO product_category_sub_categories(category_id, sub_categories_id) values (1, 2);
 INSERT INTO product_category_sub_categories(category_id, sub_categories_id) values (1, 3);
 INSERT INTO product_category_sub_categories(category_id, sub_categories_id) values (1, 4);
-=======
-
-INSERT INTO product_category(id, name, ancestor_id) values (null, 'footware', null);
-INSERT INTO product_category(id, name, ancestor_id) values (null, 'kids', 1);
-INSERT INTO product_category(id, name, ancestor_id) values (null, 'woman', 1);
-INSERT INTO product_category(id, name, ancestor_id) values (null, 'man', 1);
-
-INSERT INTO product_category_sub_categories(category_id, sub_categories_id) values (1, 2);
-INSERT INTO product_category_sub_categories(category_id, sub_categories_id) values (1, 3);
-INSERT INTO product_category_sub_categories(category_id, sub_categories_id) values (1, 4);
->>>>>>> 3a1c9345aef0c9dfb4d509b2445112f5bdd987ad
