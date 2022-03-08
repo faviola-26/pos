94e819f5fa46711eaeb1188c25a10774e264bd78
faviola-26 <faviola_jo@hotmail.com>
2022-03-08 09:22:20 -0600

    Revert "Merge origin/master"
    This reverts commit 03c5f1e8c996d2b1ec970c0ec6c071d005bb7ddb.

--- src/main/java/com/mycompany/catalog/controller/CatalogController.java
+++ src/main/java/com/mycompany/catalog/controller/CatalogController.java
@@ -5,8 +5,10 @@ import com.mycompany.catalog.model.*;
 import com.mycompany.catalog.services.*;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/catalog")
--- src/main/java/com/mycompany/catalog/repository/CharacteristicRepository.java
+++ src/main/java/com/mycompany/catalog/repository/CharacteristicRepository.java
@@ -37,7 +37,8 @@ public class CharacteristicRepository {
             manager.close();
         } 
     }
-    public Optional<Characteristic> findByName(String name) throws NoSingleResultException{
+    public Optional<Characteristic> 
+    findByName(String name) throws NoSingleResultException{
         EntityManager manager = factory.createEntityManager();
         
         var query = manager.createQuery("select C from Characteristic C where C.name = :name");
